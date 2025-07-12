d1304886b853729bfe43910e70c601ffdf4a0674
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-07-12 20:47:40 +0530

    Update Footer.jsx

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { Facebook, Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="relative z-10 w-full overflow-hidden bg-gradient-to-r from-indigo-700 via-purple-600 to-pink-600 animate-gradient-x text-white py-10 px-6">
+    <footer className="relative z-10 w-full overflow-hidden bg-gradient-to-r from-black via-black/20 to-[#5865F2] animate-gradient-x text-white py-10 px-6">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6">
         {/* Left */}
         <div className="text-center md:text-left">
